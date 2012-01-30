5eac212dff05713f653ab90f835faeb6888e1e75
Matt Tognetti <mjtognetti@gmail.com>
2012-01-29 16:39:37 -0800

    sql scripts updated. create_tables.sql now creates DATA_survey_tweets as well. insert_tweets.sql will insert tweets from survey_tweets.csv

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,3 +1,11 @@
+CREATE TABLE DATA_survey_tweets (
+   tweet_id INT NOT NULL,
+   content VARCHAR(200) NOT NULL,
+   num_ratings INT NOT NULL DEFAULT 0,
+   PRIMARY KEY (tweet_id),
+   FOREIGN KEY (tweet_id) REFERENCES DATA_tweets(id)
+);
+
 CREATE TABLE META_survey_users (
    id INT AUTO_INCREMENT NOT NULL,
    cp_login VARCHAR(10) NOT NULL,
